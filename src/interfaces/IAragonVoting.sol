2515e21ecfbd7e444222971cd2bb681b6d88efa4
Pedro Yves Fracari <yves.fracari@gmail.com>
2025-01-23 16:04:39 -0300

    chore: add aragon e2e test

--- src/interfaces/IAragonVoting.sol
+++ src/interfaces/IAragonVoting.sol
@@ -2,14 +2,6 @@
 pragma solidity ^0.8.20;
 
 interface IAragonVoting {
-    enum VoterState {
-        Absent, // Voter has not voted
-        Yea, // Voter has voted for
-        Nay, // Voter has voted against
-        DelegateYea, // Delegate has voted for on behalf of the voter
-        DelegateNay // Delegate has voted against on behalf of the voter
-    }
-
     /**
      * @notice Assign a delegate who can vote on behalf of the sender
      * @param _delegate Address of the account to delegate to
@@ -37,7 +29,7 @@ interface IAragonVoting {
     function getVoterState(
         uint256 _voteId,
         address _voter
-    ) external view returns (VoterState);
+    ) external view returns (uint8);
 
     /**
      * @notice Check if an address can vote
@@ -64,4 +56,54 @@ interface IAragonVoting {
     function getVotingPowerMultiple(
         address[] memory _voters
     ) external view returns (uint256[] memory balances);
+
+    /**
+     * @notice Create a new vote about "`_metadata`"
+     * @dev  _executesIfDecided was deprecated to introduce a proper lock period between decision and execution.
+     * @param _executionScript EVM script to be executed on approval
+     * @param _metadata Vote metadata
+     * @dev _castVote_deprecated Whether to also cast newly created vote - DEPRECATED
+     * @dev _executesIfDecided_deprecated Whether to also immediately execute newly created vote if decided - DEPRECATED
+     * @return voteId Id for newly created vote
+     */
+    function newVote(
+        bytes memory _executionScript,
+        string memory _metadata,
+        bool /* _castVote_deprecated */,
+        bool /* _executesIfDecided_deprecated */
+    ) external returns (uint256 voteId);
+
+    /**
+     * @dev Return all information for a vote by its ID
+     * @param _voteId Vote identifier
+     * @return open True if the vote is open
+     * @return executed Vote executed status
+     * @return startDate Vote start date
+     * @return snapshotBlock Vote snapshot block
+     * @return supportRequired Vote support required
+     * @return minAcceptQuorum Vote minimum acceptance quorum
+     * @return yea Vote yeas amount
+     * @return nay Vote nays amount
+     * @return votingPower Vote power
+     * @return script Vote script
+     * @return phase Vote phase
+     */
+    function getVote(
+        uint256 _voteId
+    )
+        external
+        view
+        returns (
+            bool open,
+            bool executed,
+            uint64 startDate,
+            uint64 snapshotBlock,
+            uint64 supportRequired,
+            uint64 minAcceptQuorum,
+            uint256 yea,
+            uint256 nay,
+            uint256 votingPower,
+            bytes memory script,
+            uint8 phase
+        );
 }
--- test/AragonGovernorWrapper.t.sol
+++ test/AragonGovernorWrapper.t.sol
@@ -10,12 +10,12 @@ contract AragonDelegateVaultTest is Test {
     IERC20 public token;
     address public delegatee;
 
-    // TODO: change for lido
     address constant LDO_TOKEN = 0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32;
     address constant LDO_WHALE = 0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c;
     address constant USER = address(0xbeef);
     IAragonVoting constant ARAGON_VOTING =
         IAragonVoting(0x2e59A20f205bB85a89C53f1936454680651E618e);
+    address constant CREATOR = 0xf73a1260d222f447210581DDf212D915c09a3249;
 
     uint256 constant AMOUNT = 1000 * 10 ** 18;
 
@@ -69,5 +69,42 @@ contract AragonDelegateVaultTest is Test {
 
         assertEq(ARAGON_VOTING.getVotingPowerMultiple(addresses)[0], AMOUNT);
         assertEq(ARAGON_VOTING.getDelegate(address(wrapper)), delegatee);
+    }
+
+    function test_e2e() public {
+        vm.startPrank(USER);
+        token.approve(address(wrapper), AMOUNT);
+        wrapper.deposit(AMOUNT, USER);
+        vm.stopPrank();
+
+        vm.startPrank(CREATOR);
+        bytes memory data = new bytes(32);
+        string memory metadata = "test";
+
+        vm.roll(vm.getBlockNumber() + 1);
+        uint256 voteId = ARAGON_VOTING.newVote(data, metadata, false, false);
+        vm.stopPrank();
+
+        vm.roll(vm.getBlockNumber() + 1);
+        assertEq(ARAGON_VOTING.canVote(voteId, address(wrapper)), true);
+
+        vm.startPrank(delegatee);
+        ARAGON_VOTING.attemptVoteFor(voteId, true, address(wrapper));
+
+        vm.stopPrank();
+
+        // enum VoterState {
+        //     Absent, // Voter has not voted
+        //     Yea, // Voter has voted for
+        //     Nay, // Voter has voted against
+        //     DelegateYea, // Delegate has voted for on behalf of the voter
+        //     DelegateNay // Delegate has voted against on behalf of the voter
+        // }
+
+        assertEq(ARAGON_VOTING.getVoterState(voteId, address(wrapper)), 3);
+
+        (, , , , , , uint256 yea, , , , ) = ARAGON_VOTING.getVote(voteId);
+
+        assertEq(yea, AMOUNT);
     }
 }
